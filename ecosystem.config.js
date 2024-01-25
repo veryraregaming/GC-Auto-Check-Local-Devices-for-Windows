8c0eab80491bcaae9f41743b94eea17818d1ee05
VeryRare <69217283+veryraregaming@users.noreply.github.com>
2024-01-25 22:03:55 +0000

    PM2 Support

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps : [{
+      name: 'AutoCheck',
+      script: 'run.py',
+      interpreter: 'python3',
+      watch: true, // optional: restarts the script if file changes
+      // Other configuration options...
+    }]
+  };
+  
